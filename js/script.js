d53069fae35f3847ecafb5bfb504b5847e04f3f3
Claudio-Emmolo <claudioemmolo90@gmail.com>
2022-11-29 15:06:41 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,46 @@
+// Palidroma
+// Chiedere all’utente di inserire una parola:
+// Creare una funzione per capire se la parola inserita è palindroma
+const word = prompt('Inserisci una parola');
+
+
+
+
+function checkPalindrome(string) {
+
+  const wordCalculate = string.length;
+
+  //Scorro le lettera della parole per metà della loro lunghezza ufficiale;
+  for (let i = 0; i < wordCalculate / 2; i++) {
+
+    // Controlla se la prima lettera è diversa dall'ultima;
+      if (string[i] !== string[wordCalculate - (i + 1)]) {
+          return 'Non è palindroma';
+      }
+  }
+  return 'È palindroma';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer attraverso una funzione.
+// Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari attraverso un'altra funzione
+// Dichiariamo chi ha vinto.
